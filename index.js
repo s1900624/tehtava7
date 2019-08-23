5eb79a3f4aefa75909f8d93f48b22ff8bde19146
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-23 13:38:35 +0300

    Add: confirm question

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 window.onload = function() {
-
+    const doYouKnow = confirm('Do you know a bit already about Javascrip');
+    doYouKnow === true ? console.log(':)') : console.log('Sorry ..');
 };
